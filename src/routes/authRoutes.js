22f42132cf7f70323042f2dce7502f01ae2cd9f8
parth-v <parthvashisht20@gmail.com>
2020-05-28 01:13:02 +0530

    tokenization complete

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const User = mongoose.model('User');
 
 const router = express.Router();
@@ -7,11 +8,15 @@ const router = express.Router();
 router.post('/signup', async (req, res) => {
 	
 	const { email, password } = req.body;
+	try {
+		const user = new User({ email, password });
+		await user.save();
 
-	const user = new User({ email, password });
-	await user.save();
-
-	res.send('Post req to signup!');
+		const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+		res.send({ token 	});
+	} catch(err) {
+		return res.status(422).send(err.message);
+	}
 });
 
 module.exports = router;
