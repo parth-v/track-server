91a039bebe89da430dc3a75d18b5eee7d65ae636
parth-v <parthvashisht20@gmail.com>
2020-05-27 18:05:27 +0530

    adding initial files

---
+++ src/index.js
@@ -0,0 +1,33 @@
+require('./models/User');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoURI = 'mongodb+srv://admin:admin321@cluster0-ldydp.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(mongoURI,{
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+	console.log('Connected to mongo instance!');
+});
+
+mongoose.connection.on('error', (err) => {
+	console.error('Error connecting to mongo ', err);
+});
+
+app.get('/', (req, res) => {
+	res.send('Hi there!');
+});
+
+app.listen(3000, () => {
+	console.log('Server is listening on port 3000');
+});
